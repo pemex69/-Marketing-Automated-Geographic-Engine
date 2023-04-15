899d3dd3fed8ecb1e8a4cb850eed6970d306fed8
pemex69 <randomtrtrandom@gmail.com>
2023-04-15 00:20:07 -0600

    Navbar fix, dependencias de node en package.json

--- src/API/index.js
+++ src/API/index.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
+const cookieParser = require('cookie-parser');
 const usersRoutes = require('./Users/CRUD/routes');
 const geolocationRoutes = require('./Geolocation/Map/routes');
-const app = express();
 const port = 3000;
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(compression());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-    res.send('so');
+    res.send('soo');
 });
+
+app.get ('/locationwise/setUserCookie', (req, res) => {
+    res.cookie('nombre de cookie', 'cookie value', {
+        maxAge: 1000 * 10,
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax',
+    });
+    res.send('cookie set');
+});
+
+app.get('/locationwise/getUserCookie', (req, res) => {
+    res.send('leyendo cookies . . .');
+    console.log(req.cookies);
+
+});
+
+app.get('/locationwise/deleteUserCookie', (req, res) => {
+    res.clearCookie('nombre de cookie');
+    res.send('cookie borrada');
+});
+
 app.use(express.static('public'));
 app.use('/locationwise/v1/users', usersRoutes);
 app.use('/locationwise/v1/geocode-settlement', geolocationRoutes);
---
+++ src/js/utility.js
@@ -0,0 +1,34 @@
+const viewMoreBtn = document.getElementById('viewMoreBtn');
+const moreInfo = document.getElementById('moreInfo');
+
+viewMoreBtn.addEventListener('click', () => {
+    if (moreInfo.style.display === 'none') {
+        moreInfo.style.display = 'block';
+        viewMoreBtn.innerText = 'Ver menos';
+    } else {
+        moreInfo.style.display = 'none';
+        viewMoreBtn.innerText = 'Ver más';
+    }
+});
+
+function NotRegistered() {
+    swal({
+        title: '¡Ups!',
+        text: 'Debes estar registrado para poder realizar esta acción',
+        icon: 'warning',
+        buttons: {
+            cancel: 'Cancelar',
+            confirm: 'Registrarme',
+        },
+    }).then((value) => {
+        if (value) {
+            window.location.href = './auth.html';
+        }
+    }
+    );
+
+}
+
+function geolocate() {
+    alert('tbc . . .');
+}
