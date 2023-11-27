09249e22dff86e00e9f93223c54d46faee9ad772
pemex69 <randomtrtrandom@gmail.com>
2023-11-26 20:26:35 -0600

    cors origin[] => *

--- src/API/index.js
+++ src/API/index.js
@@ -16,7 +16,6 @@ const smtp = 'https://smtpjs.com/v3/smtpjs.aspx?';
 
 const app = express();
 app.use(cors({
-    origin: [origin, smtp],
     credentials: true
 }));
 
@@ -24,7 +23,7 @@ app.use(express.json());
 app.use(compression());
 app.use(cookieParser());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
