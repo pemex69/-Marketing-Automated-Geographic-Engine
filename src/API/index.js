65f8510ed14ffec80bb0b20e2523e3159e6908f6
pemex69 <randomtrtrandom@gmail.com>
2023-11-26 20:48:46 -0600

    origin cors index.js

--- src/API/index.js
+++ src/API/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(compression());
 app.use(cookieParser());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
